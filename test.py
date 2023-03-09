97d1a757cc6831dadbb0cf24ba8555282ad6be31
Cory Frontin <cory.frontin@nrel.gov>
2023-03-09 16:23:44 -0500

    renaming refactor

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-import plot_cvf
+import tools_cvf
 
 Nplot = 201
 x = np.linspace(0, 2 * np.pi, Nplot)
@@ -22,23 +22,23 @@ def do(fname_save=None):
 
 # do()
 
-url_stylesheet_cvf = "https://github.nrel.gov/raw/cfrontin/plot_cvf/main/plot_cvf/stylesheet_cvf.mplstyle"
-url_stylesheet_cvf_notex = "https://github.nrel.gov/raw/cfrontin/plot_cvf/main/plot_cvf/stylesheet_cvf_notex.mplstyle"
-url_stylesheet_seaborn = "https://github.nrel.gov/raw/cfrontin/plot_cvf/main/plot_cvf/stylesheet_seaborn.mplstyle"
+url_stylesheet_cvf = "https://github.nrel.gov/raw/cfrontin/tools_cvf/main/tools_cvf/stylesheet_cvf.mplstyle"
+url_stylesheet_cvf_notex = "https://github.nrel.gov/raw/cfrontin/tools_cvf/main/tools_cvf/stylesheet_cvf_notex.mplstyle"
+url_stylesheet_seaborn = "https://github.nrel.gov/raw/cfrontin/tools_cvf/main/tools_cvf/stylesheet_seaborn.mplstyle"
 
 use_tex = False
 if use_tex:
     if True:
-        # with plt.style.context(plot_cvf.get_stylesheets()):
-        # with plt.style.context(['/Users/cfrontin/codes/plot_cvf/plot_cvf/stylesheet_seaborn.mplstyle',
-        #                         '/Users/cfrontin/codes/plot_cvf/plot_cvf/stylesheet_cvf.mplstyle']):
+        # with plt.style.context(tools_cvf.get_stylesheets()):
+        # with plt.style.context(['/Users/cfrontin/codes/tools_cvf/tools_cvf/stylesheet_seaborn.mplstyle',
+        #                         '/Users/cfrontin/codes/tools_cvf/tools_cvf/stylesheet_cvf.mplstyle']):
         with plt.style.context([url_stylesheet_seaborn, url_stylesheet_cvf]):
             do(fname_save="assets/plot_stylesheet.png")
     if True:
-        # with plt.style.context(plot_cvf.get_stylesheets(dark= True)):
+        # with plt.style.context(tools_cvf.get_stylesheets(dark= True)):
         # with plt.style.context(['dark_background',
-        #                         '/Users/cfrontin/codes/plot_cvf/plot_cvf/stylesheet_seaborn.mplstyle',
-        #                         '/Users/cfrontin/codes/plot_cvf/plot_cvf/stylesheet_cvf.mplstyle',]):
+        #                         '/Users/cfrontin/codes/tools_cvf/tools_cvf/stylesheet_seaborn.mplstyle',
+        #                         '/Users/cfrontin/codes/tools_cvf/tools_cvf/stylesheet_cvf.mplstyle',]):
         with plt.style.context(
             [
                 "dark_background",
@@ -49,9 +49,9 @@ if use_tex:
             do(fname_save="assets/plot_stylesheet_dark.png")
 else:
     if True:
-        with plt.style.context(plot_cvf.get_stylesheets(use_latex=False)):
+        with plt.style.context(tools_cvf.get_stylesheets(use_latex=False)):
             # with plt.style.context([url_stylesheet_seaborn, url_stylesheet_cvf_notex]):
             do(fname_save="assets/plot_stylesheet.png")
     if True:
-        with plt.style.context(plot_cvf.get_stylesheets(dark=True, use_latex=False)):
+        with plt.style.context(tools_cvf.get_stylesheets(dark=True, use_latex=False)):
             do(fname_save="assets/plot_stylesheet_dark.png")
